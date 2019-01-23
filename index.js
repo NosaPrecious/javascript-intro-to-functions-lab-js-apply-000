4fed5bf7f1dcccad62849b1d5c237e2ce0defc2c
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-01-23 00:47:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   var newVal= 'I can\'t hear you!';
-  if(newVal.toLowerCase() === newVal){
+  if(newVal.toLowerCase()){
     return 'I can\'t hear you!';
   }
 }
