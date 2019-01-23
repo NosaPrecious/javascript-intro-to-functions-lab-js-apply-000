d02a2989b1225c4ba176bf12be1166ceee816ee2
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-01-23 01:06:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function sayHiToGrandma(string){
 
   
   function sayHiToGrandma(string){
-    if(string.toLowerCase() === string){
+    if(string.toUpperCase() === string){
        return 'YES INDEED!';
     }
   }
