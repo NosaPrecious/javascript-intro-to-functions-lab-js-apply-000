90112aaafffdc807dc558b22409837abb5ce0e25
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-01-23 01:07:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,8 @@ function sayHiToGrandma(string){
   if(string.toLowerCase() === string){
     return 'I can\'t hear you!';
   }
-}
-
   
-  function sayHiToGrandma(string){
-    if(string.toUpperCase() === string){
+  if(string.toUpperCase() === string){
        return 'YES INDEED!';
     }
-  }
+}
